6c242dbd91d08c2b37db97b05592666b1442efba
Clement Lesaulnier <clemansles@gmail.com>
2024-03-19 20:08:36 +0100

    Fix 2 (#6)
    Co-authored-by: clement Lesaulnier <clement.lesaulnier@univ-grenoble-alpes.fr>

--- examples/circle.cpp
+++ examples/circle.cpp
@@ -4,9 +4,24 @@
 #include <algorithm>
 #include <fstream>
 
+#include <splinart/build_img.hpp>
+#include <splinart/imshow.hpp>
+#include <splinart/shapes.hpp>
 #include <xtensor/xmath.hpp>
 #include <xtensor/xtensor.hpp>
 
-#include <splinart/build_img.hpp>
-#include <splinart/imshow.hpp>
-usingnamespacext::placeholders;intmain(intargc,char*argv[]){std::size_timg_size=1000;std::size_tnb_samples=10000;splinart::Circlecircle({0.5,0.5},0.3,75);autoimg=splinart::build_img({img_size,img_size},{circle},nb_samples);splinart::imshow(img);return0;}
+using namespace xt::placeholders;
+
+int main(int argc, char *argv[]) {
+  std::size_t img_size = 1000;
+  std::size_t nb_samples = 10000;
+
+  std::vector<splinart::Circle> circles;
+  splinart::Circle circle({0.5, 0.5}, 0.3, 75);
+  circles.push_back(circle);
+
+  auto img = splinart::build_img({img_size, img_size}, {circles}, nb_samples);
+  splinart::imshow(img);
+
+  return 0;
+}
